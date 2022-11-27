f53656e6975dba877a8e0cc1085461b4d66bf095
fi4k <kasiitahaviera@gmail.com>
2022-11-27 06:58:56 +0300

    deploying server changing root and port

--- server.js
+++ server.js
@@ -226,8 +226,8 @@ app.get('/LoadGalleryAdmin', (req, res) => {
 
 })
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 4000
 
-app.listen(PORT || 4000, () => {
+app.listen(PORT, () => {
     console.log(`app is running on port ${PORT}!`);
 })
