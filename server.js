83cecf1de143ffe2218f50f862b0d2a484924c5b
fi4k <kasiitahaviera@gmail.com>
2022-11-27 13:19:38 +0300

    deploying server changing root and port

--- server.js
+++ server.js
@@ -24,7 +24,11 @@ app.use(bodyPaser.json());
 app.use(cors());
 app.use(expressFileupload());
 
-app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader().AllowCredentials());
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+})
 
 // const db1 = {
 //     users: [
