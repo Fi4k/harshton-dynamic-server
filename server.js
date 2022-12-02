95b4f0e5ee232b6f24c500a6babf58e12f5a5dcf
fi4k <kasiitahaviera@gmail.com>
2022-12-02 17:09:56 +0300

    gcloud knex db config

--- server.js
+++ server.js
@@ -31,101 +31,101 @@ app.use(function (req, res, next) {
 
 let pool;
 
-app.use(async (req, res, next) => {
-    if (pool) {
-        return next();
-    }
-    try {
-        pool = await createPoolAndEnsureSchema();
-        next();
-    } catch (err) {
-        // logger.error(err);
-        return next(err);
-    }
-});
+// app.use(async (req, res, next) => {
+//     if (pool) {
+//         return next();
+//     }
+//     try {
+//         pool = await createPoolAndEnsureSchema();
+//         next();
+//     } catch (err) {
+//         // logger.error(err);
+//         return next(err);
+//     }
+// });
 
-const createPool = async () => {
-    // Configure which instance and what database user to connect with.
-    // Remember - storing secrets in plaintext is potentially unsafe. Consider using
-    // something like https://cloud.google.com/kms/ to help keep secrets secret.
-    const config = { pool: {} };
+// const createPool = async () => {
+//     // Configure which instance and what database user to connect with.
+//     // Remember - storing secrets in plaintext is potentially unsafe. Consider using
+//     // something like https://cloud.google.com/kms/ to help keep secrets secret.
+//     const config = { pool: {} };
 
-    // [START cloud_sql_postgres_knex_limit]
-    // 'max' limits the total number of concurrent connections this pool will keep. Ideal
-    // values for this setting are highly variable on app design, infrastructure, and database.
-    config.pool.max = 5;
-    // 'min' is the minimum number of idle connections Knex maintains in the pool.
-    // Additional connections will be established to meet this value unless the pool is full.
-    config.pool.min = 5;
-    // [END cloud_sql_postgres_knex_limit]
+//     // [START cloud_sql_postgres_knex_limit]
+//     // 'max' limits the total number of concurrent connections this pool will keep. Ideal
+//     // values for this setting are highly variable on app design, infrastructure, and database.
+//     config.pool.max = 5;
+//     // 'min' is the minimum number of idle connections Knex maintains in the pool.
+//     // Additional connections will be established to meet this value unless the pool is full.
+//     config.pool.min = 5;
+//     // [END cloud_sql_postgres_knex_limit]
 
-    // [START cloud_sql_postgres_knex_timeout]
-    // 'acquireTimeoutMillis' is the number of milliseconds before a timeout occurs when acquiring a
-    // connection from the pool. This is slightly different from connectionTimeout, because acquiring
-    // a pool connection does not always involve making a new connection, and may include multiple retries.
-    // when making a connection
-    config.pool.acquireTimeoutMillis = 60000; // 60 seconds
-    // 'createTimeoutMillis` is the maximum number of milliseconds to wait trying to establish an
-    // initial connection before retrying.
-    // After acquireTimeoutMillis has passed, a timeout exception will be thrown.
-    config.pool.createTimeoutMillis = 30000; // 30 seconds
-    // 'idleTimeoutMillis' is the number of milliseconds a connection must sit idle in the pool
-    // and not be checked out before it is automatically closed.
-    config.pool.idleTimeoutMillis = 600000; // 10 minutes
-    // [END cloud_sql_postgres_knex_timeout]
+//     // [START cloud_sql_postgres_knex_timeout]
+//     // 'acquireTimeoutMillis' is the number of milliseconds before a timeout occurs when acquiring a
+//     // connection from the pool. This is slightly different from connectionTimeout, because acquiring
+//     // a pool connection does not always involve making a new connection, and may include multiple retries.
+//     // when making a connection
+//     config.pool.acquireTimeoutMillis = 60000; // 60 seconds
+//     // 'createTimeoutMillis` is the maximum number of milliseconds to wait trying to establish an
+//     // initial connection before retrying.
+//     // After acquireTimeoutMillis has passed, a timeout exception will be thrown.
+//     config.pool.createTimeoutMillis = 30000; // 30 seconds
+//     // 'idleTimeoutMillis' is the number of milliseconds a connection must sit idle in the pool
+//     // and not be checked out before it is automatically closed.
+//     config.pool.idleTimeoutMillis = 600000; // 10 minutes
+//     // [END cloud_sql_postgres_knex_timeout]
 
-    // [START cloud_sql_postgres_knex_backoff]
-    // 'knex' uses a built-in retry strategy which does not implement backoff.
-    // 'createRetryIntervalMillis' is how long to idle after failed connection creation before trying again
-    config.pool.createRetryIntervalMillis = 200; // 0.2 seconds
-    // [END cloud_sql_postgres_knex_backoff]
+//     // [START cloud_sql_postgres_knex_backoff]
+//     // 'knex' uses a built-in retry strategy which does not implement backoff.
+//     // 'createRetryIntervalMillis' is how long to idle after failed connection creation before trying again
+//     config.pool.createRetryIntervalMillis = 200; // 0.2 seconds
+//     // [END cloud_sql_postgres_knex_backoff]
 
-    // Check if a Secret Manager secret version is defined
-    // If a version is defined, retrieve the secret from Secret Manager and set as the DB_PASS
-    // const { CLOUD_SQL_CREDENTIALS_SECRET } = process.env;
-    // if (CLOUD_SQL_CREDENTIALS_SECRET) {
-    //     const secrets = await accessSecretVersion(CLOUD_SQL_CREDENTIALS_SECRET);
-    //     try {
-    //         process.env.DB_PASS = secrets.toString();
-    //     } catch (err) {
-    //         err.message = `Unable to parse secret from Secret Manager. Make sure that the secret is JSON formatted: \n ${err.message} `;
-    //         throw err;
-    //     }
-    // }
+//     // Check if a Secret Manager secret version is defined
+//     // If a version is defined, retrieve the secret from Secret Manager and set as the DB_PASS
+//     // const { CLOUD_SQL_CREDENTIALS_SECRET } = process.env;
+//     // if (CLOUD_SQL_CREDENTIALS_SECRET) {
+//     //     const secrets = await accessSecretVersion(CLOUD_SQL_CREDENTIALS_SECRET);
+//     //     try {
+//     //         process.env.DB_PASS = secrets.toString();
+//     //     } catch (err) {
+//     //         err.message = `Unable to parse secret from Secret Manager. Make sure that the secret is JSON formatted: \n ${err.message} `;
+//     //         throw err;
+//     //     }
+//     // }
 
-    // if (process.env.INSTANCE_HOST) {
-    // Use a TCP socket when INSTANCE_HOST (e.g., 127.0.0.1) is defined
-    return createTcpPool(config);
-    // } else if (process.env.INSTANCE_UNIX_SOCKET) {
-    // Use a Unix socket when INSTANCE_UNIX_SOCKET (e.g., /cloudsql/proj:region:instance) is defined.
-    // return createUnixSocketPool(config);
-    // } else {
-    // throw 'One of INSTANCE_HOST or INSTANCE_UNIX_SOCKET` is required.';
-    // }
-};
+//     if (process.env.INSTANCE_HOST) {
+//         // Use a TCP socket when INSTANCE_HOST (e.g., 127.0.0.1) is defined
+//         return createTcpPool(config);
+//     } else if (process.env.INSTANCE_UNIX_SOCKET) {
+//         // Use a Unix socket when INSTANCE_UNIX_SOCKET (e.g., /cloudsql/proj:region:instance) is defined.
+//         return createUnixSocketPool(config);
+//     } else {
+//         throw 'One of INSTANCE_HOST or INSTANCE_UNIX_SOCKET` is required.';
+//     }
+// };
 
-const ensureSchema = async pool => {
-    const hasTable = await pool.schema.hasTable('webgallery');
-    // if (!hasTable) {
-    //     return pool.schema.createTable('votes', table => {
-    //         table.increments('vote_id').primary();
-    //         table.timestamp('time_cast', 30).notNullable();
-    //         table.specificType('candidate', 'CHAR(6)').notNullable();
-    //     });
-    // }
-    // logger.info("Ensured that table 'votes' exists");
-};
+// const ensureSchema = async pool => {
+//     const hasTable = await pool.schema.hasTable('webgallery');
+//     // if (!hasTable) {
+//     //     return pool.schema.createTable('votes', table => {
+//     //         table.increments('vote_id').primary();
+//     //         table.timestamp('time_cast', 30).notNullable();
+//     //         table.specificType('candidate', 'CHAR(6)').notNullable();
+//     //     });
+//     // }
+//     // logger.info("Ensured that table 'votes' exists");
+// };
 
-const createPoolAndEnsureSchema = async () =>
-    await createPool()
-        .then(async pool => {
-            await ensureSchema(pool);
-            return pool;
-        })
-        .catch(err => {
-            // logger.error(err);
-            throw err;
-        });
+// const createPoolAndEnsureSchema = async () =>
+//     await createPool()
+//         .then(async pool => {
+//             await ensureSchema(pool);
+//             return pool;
+//         })
+//         .catch(err => {
+//             // logger.error(err);
+//             throw err;
+//         });
 
 // const insertVote = async (pool, vote) => {
 //     try {
@@ -226,22 +226,22 @@ const createPoolAndEnsureSchema = async () =>
 
 // createTcpPool initializes a TCP connection pool for a Cloud SQL
 // instance of Postgres.
-// const db = async config => {
-//     const dbConfig = {
-//         client: 'pg',
-//         connection: {
-//             host: process.env.INSTANCE_HOST, // e.g. '127.0.0.1'
-//             port: process.env.DB_PORT, // e.g. '5432'
-//             user: process.env.DB_USER, // e.g. 'my-user'
-//             password: process.env.DB_PASS, // e.g. 'my-user-password'
-//             database: process.env.DB_NAME, // e.g. 'my-database'
-//         },
-//         // // ... Specify additional properties here.
-//         // ...config,
-//     };
-//     // Establish a connection to the database.
-//     return Knex(dbConfig);
-// };
+const db = async config => {
+    const dbConfig = {
+        client: 'pg',
+        connection: {
+            host: process.env.INSTANCE_HOST, // e.g. '127.0.0.1'
+            port: process.env.DB_PORT, // e.g. '5432'
+            user: process.env.DB_USER, // e.g. 'my-user'
+            password: process.env.DB_PASS, // e.g. 'my-user-password'
+            database: process.env.DB_NAME, // e.g. 'my-database'
+        },
+        // // ... Specify additional properties here.
+        // ...config,
+    };
+    // Establish a connection to the database.
+    return Knex(dbConfig);
+};
 
 // console.log(db.select('*').from('gallery').then(data => { console.log(dataf) }));
 
@@ -420,8 +420,8 @@ app.get('/', (req, res) => {
 // })
 
 app.get('/LoadGalleryAdmin', (req, res) => {
-    const getGallery = async pool => {
-        return await pool
+    const getGallery = async db => {
+        return await db
             .select('*')
             .from('webgallery');
         // .orderBy('time_cast', 'desc')
