6811381e2d8d3e689686f0fe7d24cc7ddf9f68b0
fi4k <kasiitahaviera@gmail.com>
2022-12-10 23:11:45 +0300

    heroku again 6

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyPaser = require('body-parser');
 const bcrypt = require('bcrypt');
 const cors = require('cors');
 const knex = require('knex');
-const createTcpPool = require('./createTcpPool');
+// const createTcpPool = require('./createTcpPool');
 const expressFileupload = require('express-fileupload');
 
 // const db = knex({
