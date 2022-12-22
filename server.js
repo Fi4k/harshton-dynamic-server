76478136d39c96a8cc574c2dcae344850dd889f7
fi4k <kasiitahaviera@gmail.com>
2022-12-22 14:06:57 +0300

    final edits 19

--- server.js
+++ server.js
@@ -423,7 +423,7 @@ app.post('/AddGallery', async (req, res) => {
         db('webgallery').insert({ filename: fname, caption: cap, dateadded: fDate2 }).then(() => {
             console.log('added')
             res.send(JSON.stringify('added'));
-        });
+        }).catch((err) { console.log(err) });
 
     }
     catch (err) { console.log(err) }
